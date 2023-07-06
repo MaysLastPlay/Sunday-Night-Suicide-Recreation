7e78013db0efec1926a99b63a1838d064fdb3dd0
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2023-07-06 20:01:58 +0300

    OKAY BRUH

--- source/ShakingWarningSubState.hx
+++ source/ShakingWarningSubState.hx
@@ -32,7 +32,6 @@ class ShakingWarningSubState extends MusicBeatSubstate
 
     #if android
 		addVirtualPad(NONE, A_B);
-		addPadCamera();
 		#end
                 // VPAD MUST BE IN CREATE YOU'RE *******
                 super.create();
